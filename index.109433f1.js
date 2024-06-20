667db53f0d383db12ba58c36e88f1ca293aea879
Oleh Hanenko <hello.isaev@gmail.com>
2024-06-20 15:59:38 +0300

    make build

---
+++ index.109433f1.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("input").forEach(function(e){var a,l=(a=e.name.replace(/([a-z])([A-Z])/g,"$1 $2")).charAt(0).toUpperCase()+a.slice(1),c='<label for="'.concat(e.id,'" class="field-label">').concat(l,"</label>");e.insertAdjacentHTML("beforebegin",c),e.placeholder=l});
+//# sourceMappingURL=index.109433f1.js.map
